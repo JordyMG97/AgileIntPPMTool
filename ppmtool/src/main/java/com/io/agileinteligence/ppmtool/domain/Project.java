1fe78f053766ddb7180170890aef2f9fd915ee01
JordyMG97 <jordym.ibm@gmail.com>
2023-03-22 15:26:39 -0600

    Added javax.validation dependency and dependencies for each column

--- ppmtool/src/main/java/com/io/agileinteligence/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/io/agileinteligence/ppmtool/domain/Project.java
@@ -1,8 +1,10 @@
 package com.io.agileinteligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-
 import java.util.Date;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Project {
@@ -10,13 +12,21 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project Name Field is Required")
     private String projectName;
+    @NotBlank(message = "Project Identifier Field is Required")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+    @NotBlank(message = "Project Description Field is Required")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date startDate;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date endDate;
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date createdAt;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updatedAt;
 
     public Project() { }
