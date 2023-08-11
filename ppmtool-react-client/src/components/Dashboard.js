4ac1d5bd9d87393d2b518d8452f932082d94b3fe
JordyMG97 <jordym.ibm@gmail.com>
2023-08-11 16:27:53 -0600

    dashboard fron design

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -4,11 +4,21 @@ import ProjecItem from "./Project/ProjectItem";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1 className="alert alert-warning">Welcome to the Dashboard</h1>
-        <ProjecItem />
-        <ProjecItem />
-        <ProjecItem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <ProjecItem />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
