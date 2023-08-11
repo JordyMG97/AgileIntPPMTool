b136eb85d613e83cc998b388357ee921fc9ce43c
JordyMG97 <jordym.ibm@gmail.com>
2023-08-11 16:52:24 -0600

    add project form

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -3,14 +3,21 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route exact path="/dashboard" Component={Dashboard} />
+            <Route exact path="/addProject" Component={AddProject} />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
